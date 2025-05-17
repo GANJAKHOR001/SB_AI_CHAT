a42b21d398bfc9aba9bbfab2f3826b14f93ccc72
GANJAKHOR001 <angrysuraj2007@gmail.com>
2025-05-17 10:23:19 +0530

    Update repo.py

--- ChatBot/modules/repo.py
+++ ChatBot/modules/repo.py
@@ -6,13 +6,13 @@ from ChatBot import app
 async def start(_, msg):
     await msg.reply_photo(
         photo="https://i.postimg.cc/FF2Jv8D0/ec107964b90c959da231293998b6d73e.jpg",
-        caption="""Hey there, I'm Aaru, your AI chatbot. ♥︎
+        caption="""Hey there, I'm Tamanna, your AI chatbot. ♥︎
 
 If you want my bot repo, click below to get the source code.
 
 Powered by @CodeSearchDev ✨""",
         reply_markup=InlineKeyboardMarkup([
-            [InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/AsuraaSupports"),
-             InlineKeyboardButton("ʀᴇᴘᴏ", url="https://github.com/CodeSearchDev/AaruChatBot")]
+            [InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/+aB3TQoPJ2dBjZGU1"),
+             InlineKeyboardButton("ʀᴇᴘᴏ", url="https://t.me/II_SB_SIMPLE_II")]
         ])
     )
